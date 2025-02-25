62ebf82bcb0af1a7d8f7829a8fc631098c70d326
jiban123-kumar <jibandon12345@gmail.com>
2025-02-25 22:21:21 +0530

    asg

--- src/utilsFunction/cookieGenerator.js
+++ src/utilsFunction/cookieGenerator.js
@@ -3,7 +3,7 @@ const cookieGenerator = (res, token) => {
     sameSite: process.env.PRODUCTION === "true" ? "None" : "strict",
     maxAge: parseInt(process.env.COOKIE_EXPIRATION_DAYS) * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.PRODUCTION === "true",
+    secure: process.env.NODE_ENV === "production",
   });
 };
 
